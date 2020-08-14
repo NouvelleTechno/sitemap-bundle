f2a3592264805088bc431178ede71d499acd5dc6
Nouvelle-Techno.fr <contact@nouvelle-techno.fr>
2020-08-14 11:50:36 +0200

    Changed Bundle

--- SitemapBundle.php
+++ SitemapBundle.php
@@ -4,14 +4,11 @@ namespace NouvelleTechno\SitemapBundle;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
-// use Werkspot\Bundle\SitemapBundle\DependencyInjection\Compiler\ProviderCompilerPass;
 
 class SitemapBundle extends Bundle
 {
     public function build(ContainerBuilder $container)
     {
         parent::build($container);
-
-        // $container->addCompilerPass(new ProviderCompilerPass());
     }
 }
